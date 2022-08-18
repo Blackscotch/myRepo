87c89aedc6e233b2365ca91f0d0bf64534a9b198
S2116\r_korobkov <korobkov@salon2116.ru>
2022-08-18 12:24:15 +0300

    Добавили приветсвие конкретного юзера уникальным, и цикл при пустом значении юзера

--- Exp002/Program.cs
+++ Exp002/Program.cs
@@ -1,6 +1,16 @@
-﻿Console.Write("Введите ваше имя ");
-// Write() - выводит в консоль содержимое в одну строку;
-// WriteLine() - выводит содержимое с новой строки; 
+﻿// Write() - выводит в консоль содержимое в одну строку;
+// WriteLine() - выводит содержимое с новой строки;
+// ReadLine() - считывает строку из консоли;
+Console.Write("Введите имя пользователя ");
 string? username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+while(username == ""){
+    Console.Write("Введите имя пользователя ");
+    username = Console.ReadLine();
+}
+if(username?.ToLower() == "roman"){
+    Console.WriteLine("Hallo, пользователь Роман!");
+}else{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+

78f429f227f2ce9630499ac95eca6e7bc5b27b91
S2116\r_korobkov <korobkov@salon2116.ru>
2022-08-18 12:31:03 +0300

    Задачка с нахождением макс без цикла и массивов

---
+++ Exp004/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1, b = 2, c = 6, d = 10, e = 4, max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
+

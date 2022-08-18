63f942d2ee64ed18393f3252ce10e2f6e0a25e82
S2116\r_korobkov <korobkov@salon2116.ru>
2022-08-18 11:43:09 +0300

    Пример с суммой и делением чисел (инт и дабл)

--- Exp003/Program.cs
+++ Exp003/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int numberA = 3, numberB = 80; 
+// целые числа - double
+int sum = numberA + numberB;
+Console.Write("Это результат сложения numberA и numberB - ");
+Console.WriteLine(sum);
+
+double numberC = 60, numberD = 9;
+// вещественные числа (с запятой) - double
+double del = numberC / numberD;
+Console.Write("А это результат деления numberC и numberD - ");
+Console.WriteLine(del);

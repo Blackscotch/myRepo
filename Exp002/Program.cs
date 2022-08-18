477a437bf1d217475b0453d1d40d498da0f0dc97
S2116\r_korobkov <korobkov@salon2116.ru>
2022-08-18 11:18:29 +0300

    Комментарии описания команд

--- Exp002/Program.cs
+++ Exp002/Program.cs
@@ -1,4 +1,6 @@
 ﻿Console.Write("Введите ваше имя ");
+// Write() - выводит в консоль содержимое в одну строку;
+// WriteLine() - выводит содержимое с новой строки; 
 string? username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);

dcc03e211b1ddf0a6cb5c7c64e120149c7dacea5
S2116\r_korobkov <korobkov@salon2116.ru>
2022-08-25 13:26:31 +0300

    задачи по дз2

--- HW2/Exp002/Program.cs
+++ HW2/Exp002/Program.cs
@@ -3,10 +3,10 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// Метод для проверки ввода на целое число (от 0 до 9 цифр) с любым знаком. Не уверен, что это всецело вершает задачу
+// Метод для проверки ввода на целое число (от 0 до 9 цифр) с любым знаком. Не уверен, что это всецело решает задачу, 
+// например с 10-значными числами и больше
 int Input (string? data)
 {
-    // string? arg = data;
     int number;
     while(true)
     {   
@@ -23,6 +23,44 @@ int Input (string? data)
     return number;
 }
 
+// Метод выведет последнюю цифру числа, в котором от 3 до 10 цифр
+int LastNum (int data)
+{
+    if (data > 999)
+    {
+        while(data > 999)
+        {
+            data /= 10;
+        }
+        data = data % 10;
+    }
+    else
+    {
+        data = data % 10;
+    }
+    return data;
+}
+
+// Метод через логарифм. (int)Math.Log10() - вернет на 1 меньше, чем цифер в числе, потмоу + 1
+int ElseNum(int data)
+{
+    int log = (int)Math.Log10(data) + 1;
+    if(log > 3)
+    {
+        while(log > 3)
+        {
+            data /= 10;
+            log--;
+        }
+        data = data % 10;
+    }
+    else
+    {
+        data = data % 10;
+    }
+    return data;
+}
+
 string? stop = "стоп", input = "";
 
 while(stop != input?.ToLower())
@@ -32,25 +70,16 @@ while(stop != input?.ToLower())
     Console.WriteLine("Введите целое число:");
     num = Input(Console.ReadLine());
     result = Math.Abs(num);
-    if(result > 999)
-    {
-        while(Math.Abs(result) > 999)
-        {
-            result /=10;
-        }
-        result = result % 10;
-        Console.WriteLine($"Здесь число {result}");
-    }
-    else if(result >= 100 && result <= 999)
-    {
-       result = result % 10;
-       Console.WriteLine($"Здесь число {result}");
-    }
-    else if(result < 100)
+
+    if (result < 100)
     {
         Console.WriteLine($"В числе {num} третьей цифры нет");
     }
-    // result = (int)Math.Log10(Math.Abs(num)) + 1;
+    else
+    {
+        Console.WriteLine($"В числе {num} третья цифра это {LastNum(result)}");
+        Console.WriteLine($"В числе {num} третья цифра это {ElseNum(result)} в другом методе");
+    }
 
     Console.WriteLine("Напишите стоп для выхода или нажмите Enter для продолжения:");
     input = Console.ReadLine();

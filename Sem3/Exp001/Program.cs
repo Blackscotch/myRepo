062f02374dbffc6e9bdc82815fe69c6487adc329
Scotch <blackscotch@mail.ru>
2022-08-26 23:49:10 +0300

    первая решенная заада по семинару3

---
+++ Sem3/Exp001/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Seminar3
+{
+    internal class NewBaseType
+    {
+        private static void NumberOfQart(string data)
+        {
+            int quart;
+            while (true)
+            {
+                if (int.TryParse(data, out quart))
+                {
+                    if (quart > 0 && quart < 5)
+                    {
+                        Console.WriteLine($"Введено число {quart}");
+                        switch (quart)
+                        {
+                            case 1:
+                                Console.WriteLine($"В четверти {quart} возможные значения координат х и y от 0 до +бесконечность");
+                                break;
+
+                            case 2:
+                                Console.WriteLine($"В четверти {quart} возможные значения координат х от 0 до -бесконечность и y от 0 до +бесконечность");
+                                break;
+
+                            case 3:
+                                Console.WriteLine($"В четверти {quart} возможные значения координат х и y от 0 до -бесконечность");
+                                break;
+
+                            case 4:
+                                Console.WriteLine($"В четверти {quart} возможные значения координат х от 0 до +бесконечность и y от 0 до -бесконечность");
+                                break;
+                        }
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Указанной четверти не существует. Укажите четверть от 1 до 4");
+                        data = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Введите число.");
+                    data = Console.ReadLine();
+                }
+            }
+
+        }
+
+        
+    }
+
+    class TestMetods : NewBaseType
+    {
+        public static void Main(string[] args){
+            
+            Console.WriteLine("Нужно ввести номер четверти (1 - 4)");
+            NumberOfQart(Console.ReadLine());
+        }
+    }
+}
+
